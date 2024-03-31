5c85fe01781041e3cd3a376071b14c8da5a1a904
shaikhmubin02 <shaikhmubin572@gmail.com>
2024-04-01 02:08:12 +0530

    init

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,13 @@
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
+import StreamVideoProvider from '@/providers/StreamClientProvider';
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
     <main>
+      <StreamVideoProvider>
         {children}
+      </StreamVideoProvider>
     </main>
   );
 };
