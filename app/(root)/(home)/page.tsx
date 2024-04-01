5e924470f6139443fc5fe7d5296b79b49ef0a5c5
shaikhmubin02 <shaikhmubin572@gmail.com>
2024-04-01 08:49:29 +0530

    init

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,8 +2,8 @@ import MeetingTypeList from '@/components/MeetingTypeList';
 
 const Home = () => {
   const now  = new Date();
-  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+  const time = now.toLocaleTimeString('en-IN', { hour: '2-digit', minute: '2-digit' });
+  const date = (new Intl.DateTimeFormat('en-IN', { dateStyle: 'full' })).format(now);
   return (
     <section className='flex size-full flex-col gap-10 text-white '>
         <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
